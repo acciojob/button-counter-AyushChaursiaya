09a2167c255c7eb2a085f76183687634e27cdff5
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-05 23:42:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
-import './../styles/App.css';
+// import React from "react";
+// import './../styles/App.css';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+import React from 'react';
+import ButtonCounter from './ButtonCounter';
+
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <ButtonCounter />
     </div>
-  )
+  );
 }
+
+export default App;
