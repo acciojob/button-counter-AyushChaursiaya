0988b6295d8d2e2af14c353cd5046a114d0c2f39
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-03 06:13:43 +0000

    updating files

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@
 // }
 
 // import React from 'react';
-import ButtonCounter from './ButtonCounter';
+import ButtonCounter from "../ButtonCounter";
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { render } from "enzyme";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM(document.getElementById("root"));
+
+root.render(<>
+<App/>
+</>)
