226adbb1557e63e2acf3aae19a8d643aa2499d1b
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-10 18:28:51 +0000

    file

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,5 @@
 
-// import React from "react";
-// import './../styles/App.css';
-
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
+import React from "react";
 
 const App = () => {
     const [count, setCount] = useState(0);
