ee068b8b1d66976e861f403e3bff4479a810ebb8
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-10 18:13:41 +0000

    abcd

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from "./components/App";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<>
-<App/>
-</>)
+// root.render(<>
+// <App/>
+// </>)
+
+ReactDOM.render(<App/> , document.getElementById("root"));
